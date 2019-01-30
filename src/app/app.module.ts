4bbd27575afd01c80fc18f0777fd0fb23d085506
Yihao Jiang <Njuhobby@users.noreply.github.com>
2019-01-30 17:10:28 +0800

    new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {Routes, RouterModule} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TableComponent } from './table/table.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+
+const routes:Routes=[
+  {
+    path:'', 
+    component:AdminLayoutComponent, 
+    children:[
+      {
+        path:'',
+        loadChildren:''
+      }
+    ]
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    TableComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,

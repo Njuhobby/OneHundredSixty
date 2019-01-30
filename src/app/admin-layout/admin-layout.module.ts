66385c9c2987a228a1a45bc013ba43640abef604
Yihao Jiang <Njuhobby@users.noreply.github.com>
2019-01-30 17:39:46 +0800

    new changes 2

--- src/app/admin-layout/admin-layout.module.ts
+++ src/app/admin-layout/admin-layout.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AdminLayoutRoutes } from'./admin-layout.routing';
+import { DashboardComponent} from '../dashboard/dashboard.component';
+import { TableComponent } from '../table/table.component';
+import { UserProfileComponent } from '../user-profile/user-profile.component';
+
+import { 
+  MatButtonModule, 
+  MatInputModule, 
+  MatRippleModule, 
+  MatFormFieldModule, 
+  MatTooltipModule, 
+  MatSelectModule
+} from '@angular/material';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardComponent, 
+    TableComponent, 
+    UserProfileComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule, 
+    RouterModule.forChild(AdminLayoutRoutes), 
+    MatButtonModule, 
+    MatButtonModule,
+    MatRippleModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatTooltipModule
   ]
 })
 export class AdminLayoutModule { }
--- src/app/admin-layout/admin-layout.routing.ts
+++ src/app/admin-layout/admin-layout.routing.ts
@@ -1,1 +1,10 @@
-import{Routes} from '@angular/route'
+import{Routes} from '@angular/router';
+import{DashboardComponent} from '../dashboard/dashboard.component';
+import{TableComponent} from '../table/table.component';
+import{UserProfileComponent} from '../user-profile/user-profile.component'
+
+export const AdminLayoutRoutes:Routes = [
+    {path:'dashboard', component:DashboardComponent},
+    {path:'user-profile', component:UserProfileComponent}, 
+    {path:'table', component:TableComponent}
+];

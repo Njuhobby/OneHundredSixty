3f614de3b6cddb90b7b25690f021b163f9d89c5a
Yihao Jiang <Njuhobby@users.noreply.github.com>
2019-01-31 00:31:53 +0800

    change more

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
@@ -8,18 +7,6 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 
-const routes:Routes=[
-  {
-    path:'', 
-    component:AdminLayoutComponent, 
-    children:[
-      {
-        path:'',
-        loadChildren:'./admin-layout/admin-layout.module#AdminLayoutModule'
-      }
-    ]
-  }
-];
 
 @NgModule({
   declarations: [

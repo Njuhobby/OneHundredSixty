c1f2f9867ffbac1c22bae08955ac2432d27adf98
Yihao Jiang <Njuhobby@users.noreply.github.com>
2019-02-07 01:42:31 +0800

    10

--- src/app/admin-layout/admin-layout.component.ts
+++ src/app/admin-layout/admin-layout.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd, NavigationStart } from '@angular/router';
+import { Location, PopStateEvent } from '@angular/common';
+import PerfectScrollbar from 'perfect-scrollbar';
 
 @Component({
   selector: 'app-admin-layout',
@@ -6,10 +9,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-layout.component.scss']
 })
 export class AdminLayoutComponent implements OnInit {
+  private lastPoppedUrl: string;
+  private yScrollStack: number[] = [];
 
-  constructor() { }
+  constructor(public location: Location, private router: Router) { }
 
   ngOnInit() {
+    const isWindows = navigator.platform.indexOf('Win') > -1 ? true : false;
+
+    if (isWindows && !document.getElementsByTagName('body')[0].classList.contains('sidebar-mini')) {
+      document.getElementsByTagName('body')[0].classList.add('perfect-scrollbar-on');
+    } else {
+      document.getElementsByTagName('body')[0].classList.remove('perfect-scrollbar-off');
+    }
+    const elemMainPanel = <HTMLElement>document.querySelector('.main-panel');
+    const elemSidebar = <HTMLElement>document.querySelector('.sidebar .sidebar-wrapper');
+
+    this.location.subscribe((ev: PopStateEvent) => {
+      this.lastPoppedUrl = ev.url;
+    });
+    this.router.events.subscribe((event: any) => {
+      if (event instanceof NavigationStart) {
+        if (event.url != this.lastPoppedUrl)
+          this.yScrollStack.push(window.scrollY);
+      } else if (event instanceof NavigationEnd) {
+        if (event.url == this.lastPoppedUrl) {
+          this.lastPoppedUrl = undefined;
+          window.scrollTo(0, this.yScrollStack.pop());
+        } else
+          window.scrollTo(0, 0);
+      }
+    });
+
+    if (!this.isMac()) {
+      let ps = new PerfectScrollbar(elemMainPanel);
+      ps = new PerfectScrollbar(elemSidebar);
+    }
   }
 
+  isMac(): boolean {
+    let bool = false;
+    if (navigator.platform.toUpperCase().indexOf('MAC') >= 0 || navigator.platform.toUpperCase().indexOf('IPAD') >= 0) {
+      bool = true;
+    }
+    return bool;
+  }
 }
